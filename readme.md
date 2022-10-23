21fca3fdab4d9978f6f0614e5c3303cea9575e05
cookiedan42 <cookiedan42@gmail.com>
2022-10-23 16:17:01 +0800

    add username pseudo-db

--- readme.md
+++ readme.md
@@ -6,3 +6,4 @@ two get endpoints, one for admin only, one for all to use
 when not logged in --> ie with no valid JWT, all are unauthenticated  
 when logged in as normal user --> only can get 200 from allFunc  
 when logged in as admin --> get 200 from both get endpoints  
+authentication is based on username passed
--- user-service/controller/user-controller.js
+++ user-service/controller/user-controller.js
@@ -1,15 +1,21 @@
 import { SignJWT, jwtVerify, generateKeyPair, } from 'jose'
-import {unauthorized, unauthenticated,ok} from './response.js'
+import { unauthorized, unauthenticated, ok } from './response.js'
 //import 'dotenv/config'
 
 const { publicKey, privateKey } = await generateKeyPair('ES256')
 
+const adminAccounts = {
+  'admin': 'adminPW',
+}
+const userAccounts = {
+  'user': 'userPW',
+}
+
+
+
 async function checkJWT(req) {
-  console.log("pre header") 
-  console.log(req.headers)
-  console.log("post header")
   try {
-    if (!req.headers.cookie) { return false; }    
+    if (!req.headers.cookie) { return false; }
   } catch (error) {
     console.log(error)
     return false;
@@ -19,7 +25,7 @@ async function checkJWT(req) {
   if (cookies.length != 1) { return false }
   let jwt_received = cookies[0].split("=")[1]
   try {
-    const { payload, protectedHeader } = await jwtVerify(jwt_received, publicKey, { issuer: 'user-service'})
+    const { payload, protectedHeader } = await jwtVerify(jwt_received, publicKey, { issuer: 'user-service' })
     return payload
   } catch (err) {
     console.log("invalid jwt")
@@ -28,20 +34,29 @@ async function checkJWT(req) {
 }
 
 export async function login(req, res) {
-    //password salt and hash here
-    let { username, password } = req.body;
+  //password salt and hash here
+  let { username, password } = req.body;
 
-    let isAdmin = false;
-    if (username === "admin"){isAdmin=true}
+  let isAdmin = false;
 
-    let jwt_cookie = await new SignJWT({ 'isAdmin': isAdmin })
-      .setProtectedHeader({ alg: 'ES256' })
-      .setIssuedAt()
-      .setIssuer('user-service')
-      .setExpirationTime('2h')
-      .sign(privateKey)
-    res.cookie('JWT', jwt_cookie,)// { httpOnly: true, secure: process.env.ENV == "PROD" });
-    return res.status(201).json({ message: `Successful login as ${username} !` });
+  if (userAccounts[username] != null) {
+    isAdmin = false
+  }
+  else if (adminAccounts[username] != null) {
+    isAdmin = true
+  }
+  else {
+    return res.status(401).json({ message: `user ${username} does not exist!` });
+  }
+
+  let jwt_cookie = await new SignJWT({ 'isAdmin': isAdmin })
+    .setProtectedHeader({ alg: 'ES256' })
+    .setIssuedAt()
+    .setIssuer('user-service')
+    .setExpirationTime('2h')
+    .sign(privateKey)
+  res.cookie('JWT', jwt_cookie,)// { httpOnly: true, secure: process.env.ENV == "PROD" });
+  return res.status(201).json({ message: `Successful login as ${username} !` });
 }
 
 export async function logout(req, res) {
@@ -49,21 +64,19 @@ export async function logout(req, res) {
   return res.status(201).json({ message: `Successful logout!` });
 }
 
-
-
-export async function adminFunc(req,res) {
+export async function adminFunc(req, res) {
   let payload = await checkJWT(req);
   if (payload === false) { return unauthenticated(res) }
-  if (!payload.isAdmin) {return unauthorized(res)}
+  if (!payload.isAdmin) { return unauthorized(res) }
   return ok(res)
 }
 
-export async function allFunc(req,res) {
+export async function allFunc(req, res) {
   let payload = await checkJWT(req);
   console.log(payload)
-  if (payload === false) { 
+  if (payload === false) {
     console.log("hi")
-    return unauthenticated(res) 
+    return unauthenticated(res)
   }
   return ok(res)
 }
